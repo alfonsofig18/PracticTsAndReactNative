33cdc66f778a813d60e6b095adf173c5560e7a52
alfonso <alfonsofigueroa1515@gmail.com>
2024-08-02 18:23:52 -0500

    ultimos

--- src/typescript/BasicTypes.tsx
+++ src/typescript/BasicTypes.tsx
@@ -6,15 +6,24 @@ export const BasicTypes = () => {
         name: string;
     }
 
-    const name: Person = {
+    const person: Person = {
         id: 123456,
         name: "Juan José"
     };
 
-  return (
-    <>
-    <h3>Tipos básicos</h3>
-    {name.id}
-    </>
-  )
+    const age: number = 3;
+    const isActive: boolean = true;
+    const name = ['Alfonso', 'José', 'Figueroa', 88, true];
+    const powers: string[] = ['Fuerza', 'Volar', 'Levitar']
+
+
+    return (
+        <>
+            <h2>Tipos básicos</h2>
+            <h3>Objetos</h3>
+            <span style={{ color: 'red' }}>Identificación: {person.id}</span>
+            <br />
+            <span style={{ color: 'red' }}>Nombre: {person.name}</span>
+        </>
+    )
 }
