ba1abd3e274d2d79cf46b642aed4248bc8f9f7f2
Alfonso <alfonsofigueroa1515@gmail.com>
2024-08-06 09:30:06 -0500

    hook

--- src/components/UsersPage.tsx
+++ src/components/UsersPage.tsx
@@ -1,50 +1,5 @@
-import axios from "axios";
-import { useEffect, useRef, useState } from "react";
-import type { ReqResUserListResponse, User } from "../interfaces";
-
-const loadUsers = async (page: number = 1): Promise<User[]> => {
-    try {
-        const { data } = await axios<ReqResUserListResponse>('https://reqres.in/api/users', {
-            method: 'GET',
-            params: {
-                page: page
-            }
-        });
-        return data.data;
-    } catch (error) {
-        console.error(error);
-        return [];
-    }
-}
 
 export const UsersPage = () => {
-
-    const [users, setUsers] = useState<User[]>([]);
-    const currentPageRef = useRef<number>(1);
-
-    useEffect(() => {
-        loadUsers(currentPageRef.current).then((usuarios) => setUsers(usuarios));
-        // Tambien sería
-        // loadUsers().then(setUsers);
-    }, [])
-
-    const nextPage = async () => {
-        currentPageRef.current++;
-        const users: User[] = await loadUsers(currentPageRef.current);
-        if (users.length > 0) {
-            setUsers(users);
-        } else {
-            currentPageRef.current--;
-        }
-    }
-
-    const prevPage = async () => {
-        if (currentPageRef.current < 1) return;
-
-        currentPageRef.current--;
-        const users: User[] = await loadUsers(currentPageRef.current);
-        setUsers(users);
-    }
 
     return (
         <>
--- src/hooks/useUsers.tsx
+++ src/hooks/useUsers.tsx
@@ -1,18 +1,61 @@
-import React, { useRef, useState } from 'react'
-import { User } from '../interfaces';
+import { useRef, useState, useEffect } from 'react';
+import axios from 'axios';
+import type { ReqResUserListResponse, User } from '../interfaces';
 
-interface Props {
-    numPage: number;
-    allUsers: User[];
+
+
+const loadUsers = async (page: number = 1): Promise<User[]> => {
+    try {
+        const { data } = await axios<ReqResUserListResponse>('https://reqres.in/api/users', {
+            method: 'GET',
+            params: {
+                page: page
+            }
+        });
+        return data.data;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
 }
 
-const useUsers = ({ numPage = 1, allUsers }: Props) => {
-    const [users, setUsers] = useState<User[]>(allUsers);
-    const currentPageRef = useRef<number>(numPage);
+const useUsers = () => {
+    const [users, setUsers] = useState<User[]>([]);
+    const currentPageRef = useRef<number>(1);
+
+    useEffect(() => {
+        loadUsers(currentPageRef.current).then((usuarios) => setUsers(usuarios));
+        // Tambien sería
+        // loadUsers().then(setUsers);
+    }, [])
+
+    const nextPage = async () => {
+        currentPageRef.current++;
+        const users: User[] = await loadUsers(currentPageRef.current);
+        if (users.length > 0) {
+            setUsers(users);
+        } else {
+            currentPageRef.current--;
+        }
+    }
+
+    const prevPage = async () => {
+        if (currentPageRef.current < 1) return;
+
+        currentPageRef.current--;
+        const users: User[] = await loadUsers(currentPageRef.current);
+        setUsers(users);
+    }
 
     return {
+        //Properties
         users,
+
+        //Methods
+        nextPage,
+        prevPage,
     }
+
 }
 
 export default useUsers
